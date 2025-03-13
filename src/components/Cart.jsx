10fb1d2978b1470543e1dcf4308fc74d46590aed
Renato Chocce Palacios <renatochocce758@gmail.com>
2025-03-13 18:41:06 -0300

    use effect en cart y estilos en cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { CartContext } from './Context/CartContext';
 import { Link } from 'react-router-dom'; // Si estás usando React Router
 import { CartItem } from './CartItem';
 
 export const Cart = () => {
     const { cart, clearCart, total } = useContext(CartContext);
+
+    useEffect( () => {
+        document.body.classList.add('bg-[#ededed]');
+        return () => {
+            document.body.classList.remove('bg-[#ededed]');
+        }
+    }, [])
+
     console.log(cart)
     if (cart.length === 0) {
         return (
@@ -16,17 +24,26 @@ export const Cart = () => {
     }
 
     return (
-        <div className='mt-4 grid grid-cols-2 gap-4'>
-            <div>
+        <div className='mt-4  px-[2rem] '>
+            <div className='grid grid-cols-1 sm:grid-cols-3 gap-4 max-w-[1000px] mx-auto'>
+                <div className='bg-white sm:col-span-2'>
+                    <h1 className='py-[1.1rem] px-[2rem] text-2xl '>Carrito De Compras</h1>
+                    {cart.map(producto => <CartItem key={producto.id} item={producto} />)}
+                </div>
 
-            {cart.map(producto => <CartItem key={producto.id} item={producto} />)}
-            </div>
+                <div className='shadow-[0_1px_2px_rgba(0,0,0,0.12)] flex flex-col gap-2 justify-between  bg-white p-[1.2rem]'>
+                    <h3 className='font-semibold py-3.5 border-b-1'>Resumen de compra</h3>
+                    <div>
+                        <h4 className='flex justify-between'>Productos  <div>{cart.length}</div></h4>
+                        <h4 className='flex justify-between'>Total  <div>${total}</div></h4></div>
+                    <div className='py-3.5 flex gap-2 flex-col'>
+                        <div className='flex flex-col text-center '>
+                            <button onClick={() => clearCart()} className='py-[0.5rem] bg-blue-500 text-white cursor-pointer'>Limpiar carrito</button>
+                            <Link to='/checkout' className='py-[0.5rem] bg-black text-white cursor-pointer'>Terminar Compra</Link>
+                        </div>
+                    </div>
 
-            <div>
-                <button onClick={() => clearCart()} >Limpiar carrito</button>
-                <Link to='/checkout' >Checkout </Link>
-            <h2>total {total}</h2>
-
+                </div>
             </div>
         </div>
     );
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -20,17 +20,15 @@ export const CartItem = ({ item }) => {
 
 
             <article
-                className="flex flex-col sm:flex-row justify-center min-h-full gap-[2rem] bg-white rounded-lg text-[clamp(0.1rem,5vw,1rem)]">
-                <figure className="overflow-hidden col-span-1 w-[40%]">
+                className="flex justify-between  p-[2rem] gap-[2rem]  rounded-lg text-[clamp(0.1rem,5vw,1rem)]">
+                <figure className="overflow-hidden col-span-1 w-[200px]">
                     <img src={item.imagen} alt="" className="hover:scale-[1.2] transition duration-500 ease-in-out" />
                 </figure>
                 <div className="flex flex-col justify-center gap-3 col-span-2 w-[50%]">
                     <div className="flex flex-col gap-[10px]">
                         <h2 className="font-medium text-[1.3rem]">{item.nombre}</h2>
-                        <p className="text-gray-600">{item.descripcion}</p>
                         <p>cantidad: {item.cantidad}</p>
-                        <p>$ {item.precio}</p>
-                        <p>Subtotal: {(item.cantidad * item.precio).toFixed(2)}</p>
+                        <p> $ {(item.cantidad * item.precio).toFixed(2)}</p>
                         <button onClick={() => removeItem(item.id)}>Eliminar</button>
                     </div>
                     {/* <Link className="text-white bg-black w-full py-2 px-3 rounded-[1.8rem] cursor-pointer text-center">Agregar al carrito</Link> */}
