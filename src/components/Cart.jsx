661a431906071812557e413e79c9625478abda31
Renato Chocce Palacios <renatochocce758@gmail.com>
2025-03-17 22:34:06 -0300

    estilos en cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -24,14 +24,14 @@ export const Cart = () => {
     }
 
     return (
-        <div className='mt-[9rem]  px-[2rem] '>
+        <div className='mt-[9rem] mb-[9rem]  px-[2rem] '>
             <div className='grid md:grid-cols-3 overflow-visible gap-4 max-w-[1000px] mx-auto'>
                 <div className='md:col-span-2 bg-white sm:col-span-2 shadow-[0_1px_2px_rgba(0,0,0,0.12)]'>
-                    <h1 className='py-[1.1rem] px-[2rem] text-lg'>Carrito De Compras</h1>
-                    {cart.map(producto => <CartItem key={producto.id} item={producto} />)}
+                    {/* <h1 className='py-[1.1rem] px-[2rem] text-lg'>Carrito De Compras</h1> */}
+                    {cart.map((producto, index) => <CartItem key={producto.id} item={producto} index={index} />)}
                 </div>
 
-                <div className="md:col-span-1 self-start sticky top-[8rem] ">
+                <div className="col-span md:col-span-1 self-start sticky top-[8rem] ">
                     <div className="p-6 bg-white shadow-[0_1px_2px_rgba(0,0,0,0.12)] flex flex-col gap-2 ">
                         <h3 className="font-semibold text-lg border-gray-300 ">Resumen de compra</h3>
                         <div className="flex flex-col gap-2">
@@ -39,7 +39,7 @@ export const Cart = () => {
                             <h4 className="flex justify-between text-lg font-bold">Total <span>${total}</span></h4>
                         </div>
                         <div className="flex flex-col space-y-2">
-                            <button onClick={clearCart} className="py-3 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition">
+                            <button onClick={clearCart} className="py-3 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition cursor-pointer">
                                 Limpiar carrito
                             </button>
                             <Link to='/checkout' className="py-3 bg-blue-500 text-white font-semibold rounded-md text-center hover:bg-blue-600 transition">
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { CartContext } from './Context/CartContext'
 
-export const CartItem = ({ item }) => {
+export const CartItem = ({ item, index }) => {
 
     const { removeItem } = useContext(CartContext);
 
@@ -19,17 +19,17 @@ export const CartItem = ({ item }) => {
 
 
 
-            <article
-                className="flex justify-between  p-[2rem] gap-[2rem]  rounded-lg text-[clamp(0.1rem,5vw,1rem)]">
-                <figure className="overflow-hidden col-span-1 w-[200px]">
+            <article  className={`grid grid-cols-1 md:grid-cols-2  p-[2rem] gap-[2rem] text-[clamp(0.1rem,5vw,1rem)] 
+                ${index > 0 ? 'border-t-[1px] border-gray-300' : ''}`}>
+                <figure className="overflow-hidden col-span-1">
                     <img src={item.imagen} alt="" className="hover:scale-[1.2] transition duration-500 ease-in-out" />
                 </figure>
-                <div className="flex flex-col justify-center gap-3 col-span-2 w-[50%]">
+                <div className="flex flex-col justify-center gap-3 col-span-1 ">
                     <div className="flex flex-col gap-[10px]">
                         <h2 className="font-medium text-[1.3rem]">{item.nombre}</h2>
-                        <p>cantidad: {item.cantidad}</p>
+                        <p>Cantidad: {item.cantidad}</p>
                         <p> $ {(item.cantidad * item.precio).toFixed(2)}</p>
-                        <button onClick={() => removeItem(item.id)}>Eliminar</button>
+                        <button onClick={() => removeItem(item.id)} className='bg-red-400 p-[10px] text-white cursor-pointer'>Eliminar</button>
                     </div>
                     {/* <Link className="text-white bg-black w-full py-2 px-3 rounded-[1.8rem] cursor-pointer text-center">Agregar al carrito</Link> */}
 
